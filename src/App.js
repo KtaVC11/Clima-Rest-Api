bdcdfc5bca2c159dc42516e86a59c088a419dbef
KtaVC11 <55267352+KtaVC11@users.noreply.github.com>
2020-07-22 21:15:02 -0600

    final

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 import Error from './components/Error';
+import Clima from './components/Clima';
 
 
 class App extends Component {
@@ -12,8 +13,17 @@ class App extends Component {
 
   }
 
-  componentDidUpdate() {
-    this.consultarApi();
+  componentDidUpdate(prevProps, prevState) {
+    /*console.log('Props')
+    console.log(prevProps);
+    console.log('State')
+    console.log(prevState);*/
+    //si el state antetior es distinto del de la consulta, que realice la consulta al api
+    //si los datos son distintos que haga la consulta para que no este haciendo la consulta muchas veces innecesarias
+    if (prevState.consulta !== this.state.consulta) {
+      this.consultarApi();
+    }
+
   }
 
   componentDidMount() {
@@ -28,6 +38,8 @@ class App extends Component {
     //console.log(ciudad);
 
     //leer la url y agregar el API key
+    //const appId = '451fd66a909484d911e8f2d1f98f6257;'
+    // let url=`https://api.darksky.net/forecast`
     const appId = '0d33f7c266099e701156d0da07efa3a6';
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&APPID=${appId}`;
     // console.log(url);
@@ -63,7 +75,7 @@ class App extends Component {
     } else {
       //console.log('todo correcto');
       this.setState({
-        consulta: respuesta
+        consulta: respuesta, error: false
       })
 
     }
@@ -77,7 +89,7 @@ class App extends Component {
     if (error) {
       resultado = <Error mensaje='Ambos campos son obligatorios' />
     } else {
-
+      resultado = <Clima resultado={this.state.resultado} />
     }
 
     return (
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Formulario extends Component {
     //leer datos del formulario
@@ -62,6 +63,10 @@ class Formulario extends Component {
             </div>
         );
     }
+}
+
+Formulario.propTypes = {
+    datosConsulta: PropTypes.func.isRequired
 }
 
 export default Formulario;
